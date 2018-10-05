1c474b2fbebb248277373fa4c984fbc4db06a3e6
Steve Smith <tarkasteve@gmail.com>
2018-10-06 09:57:56 +1000

    Move run operations into common code.

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,22 +3,24 @@ use failure::{Error};
 
 use std::fs::{File};
 use std::io::{Read, Write};
-use std::process::Command;
+use std::process::{Output};
 use tempfile::tempdir;
 use escargot::{CargoBuild};
 
-fn get_bin() -> Result<Command, Error>  {
-    let bin = CargoBuild::new()
+
+fn run(args: &[&str]) -> Result<Output, Error> {
+    let out = CargoBuild::new()
         .run()?
-        .command();
-    Ok(bin)
+        .command()
+        .args(args)
+        .output()?;
+    Ok(out)
 }
+
 
 #[test]
 fn basic_help() -> Result<(), Error>  {
-    let out = get_bin()?
-        .arg("--help")
-        .output()?;
+    let out = run(&["--help"])?;
 
     assert!(out.status.success());
 
@@ -31,8 +33,7 @@ fn basic_help() -> Result<(), Error>  {
 
 #[test]
 fn no_args() -> Result<(), Error>  {
-    let out = get_bin()?
-        .output()?;
+    let out = run(&[])?;
 
     assert!(!out.status.success());
     assert!(out.status.code().unwrap() == 1);
@@ -45,10 +46,8 @@ fn no_args() -> Result<(), Error>  {
 
 #[test]
 fn source_missing() -> Result<(), Error>  {
-    let out = get_bin()?
-        .arg("/this/should/not/exist")
-        .arg("/dev/null")
-        .output()?;
+    let out = run(&["/this/should/not/exist",
+                    "/dev/null"])?;
 
     assert!(!out.status.success());
     assert!(out.status.code().unwrap() == 1);
@@ -69,11 +68,9 @@ fn dest_file_exists() -> Result<(), Error>  {
         File::create(&source_path)?;
         File::create(&dest_path)?;
     }
-    let out = get_bin()?
-        .arg("--no-clobber")
-        .arg(source_path.as_os_str())
-        .arg(dest_path.as_os_str())
-        .output()?;
+    let out = run(&["--no-clobber",
+                    source_path.to_str().unwrap(),
+                    dest_path.to_str().unwrap()])?;
 
     assert!(!out.status.success());
     let stderr = String::from_utf8(out.stderr)?;
@@ -92,11 +89,9 @@ fn dest_file_in_dir_exists() -> Result<(), Error>  {
         File::create(&dir.path().join("dest.txt"))?;
     }
 
-    let out = get_bin()?
-        .arg("--no-clobber")
-        .arg(source_path.as_os_str())
-        .arg(dir.path().as_os_str())
-        .output()?;
+    let out = run(&["--no-clobber",
+                    source_path.to_str().unwrap(),
+                    dir.path().to_str().unwrap()])?;
 
     assert!(!out.status.success());
     let stderr = String::from_utf8(out.stderr)?;
@@ -117,10 +112,8 @@ fn file_copy() -> Result<(), Error>  {
         write!(&source, "{}", text);
     }
 
-    let out = get_bin()?
-        .arg(source_path.as_os_str())
-        .arg(dest_path.as_os_str())
-        .output()?;
+    let out = run(&[source_path.to_str().unwrap(),
+                    dest_path.to_str().unwrap()])?;
 
     assert!(out.status.success());
 
