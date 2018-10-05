2bd1051fb93ec140c7a017fd80324695c63581b7
Steve Smith <tarkasteve@gmail.com>
2018-10-05 14:23:22 +1000

    Add util for fetching binary and add no-args test.

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,13 +4,17 @@ use failure::{Error};
 use std::process::Command;
 use escargot::{CargoBuild, CargoError};
 
-#[test]
-fn basic() -> Result<(), Error>  {
-    let mut bin = CargoBuild::new()
+fn get_bin() -> Result<Command, Error>  {
+    let bin = CargoBuild::new()
         .run()?
         .command();
+    Ok(bin)
+}
 
-    let out = bin
+#[test]
+fn basic_help() -> Result<(), Error>  {
+
+    let out = get_bin()?
         .arg("--help")
         .output()?;
 
@@ -18,6 +22,22 @@ fn basic() -> Result<(), Error>  {
 
     let stdout = String::from_utf8(out.stdout)?;
     assert!(stdout.contains("Copy SOURCE to DEST"));
+
+    Ok(())
+}
+
+
+#[test]
+fn no_args() -> Result<(), Error>  {
+
+    let out = get_bin()?
+        .output()?;
+
+    assert!(!out.status.success());
+    assert!(out.status.code().unwrap() == 1);
+
+    let stderr = String::from_utf8(out.stderr)?;
+    assert!(stderr.contains("The following required arguments were not provided"));
 
     Ok(())
 }
