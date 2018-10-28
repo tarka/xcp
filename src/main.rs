77fed17203145303444de122c47e2b914015e07a
Steve Smith <tarkasteve@gmail.com>
2018-10-28 17:09:42 +1100

    Lock to GPLv3.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@
  * Copyright © 2018, Steve Smith <tarkasteve@gmail.com>
  *
  * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * modify it under the terms of the GNU General Public License version
+ * 3 as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
