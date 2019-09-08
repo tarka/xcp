2d11dfcf84efc8f1ef14b92533652620bb1f03f7
Steve Smith <tarkasteve@gmail.com>
2019-09-08 12:57:54 +1000

    Initial multi-threaded single-file copy test. Slightly hacky for now.

--- src/drivers/parblock.rs
+++ src/drivers/parblock.rs
@@ -16,16 +16,18 @@
 
 use crossbeam_channel as cbc;
 use log::{debug, error, info};
-use std::fs::{create_dir_all, read_link};
+use std::cmp;
+use std::fs::{File, OpenOptions, create_dir_all, read_link};
 use std::io::ErrorKind as IOKind;
 use std::os::unix::fs::symlink;
 use std::path::{PathBuf};
+use std::sync::Arc;
 use std::thread;
 use walkdir::{WalkDir};
 
 use crate::errors::{io_err, Result, XcpError};
 use crate::drivers::{CopyDriver};
-use crate::operations::{copy_file};
+use crate::os::{allocate_file, copy_file_offset};
 use crate::progress::{
     iprogress_bar, BatchUpdater, NopUpdater, ProgressBar, ProgressUpdater, StatusUpdate, Updater,
     BATCH_DEFAULT,
@@ -45,6 +47,7 @@ impl CopyDriver for Driver {
     }
 
     fn copy_single(&self, source: &PathBuf, dest: PathBuf, opts: &Opts) -> Result<()> {
+        debug!("CALLING SINGLE");
         copy_single_file(source, dest, opts)
     }
 }
@@ -52,12 +55,85 @@ impl CopyDriver for Driver {
 
 // ********************************************************************** //
 
+#[derive(Debug)]
+struct CopyOp {
+    from: PathBuf,
+    to: PathBuf,
+    start: u64,
+    bytes: u64,
+}
+
+fn copy_worker(work: cbc::Receiver<CopyOp>) -> Result<()> {
+    debug!("Starting copy worker {:?}", thread::current().id());
+    for op in work {
+        debug!("Received operation {:?}", op);
+        info!("Worker: Copy {:?}", op);
+
+        {
+            let from = File::open(&op.from)?;
+            let to = OpenOptions::new()
+                .write(true)
+                .append(false)
+                .open(&&op.to)?;
+
+            let r = copy_file_offset(&from, &to, op.bytes, op.start as i64);
+            if !r.is_ok() {
+                error!("Error copying: {:?}", r);
+                r?;
+            }
+        }
+
+    }
+    debug!("Copy worker {:?} shutting down", thread::current().id());
+    Ok(())
+}
+
+
+
+pub fn copy_single_file(source: &PathBuf, dest: PathBuf, opts: &Opts) -> Result<()> {
+    debug!("IN SINGLE");
+
+    let nworkers = num_workers(&opts);
+    let (work_tx, work_rx) = cbc::unbounded();
+
+    let mut thandles = Vec::with_capacity(nworkers as usize);
+    for _ in 0..nworkers {
+        let worker = {
+            let wrx = work_rx.clone();
+            thread::spawn(|| copy_worker(wrx))
+        };
+        thandles.push(worker);
+    }
+
+    let bsize = 1024*1024;
+
+    let len = source.metadata()?.len();
+    let blocks = (len / bsize) + (if len % bsize > 0 { 1 } else { 0 });
+
+    let outfd = File::create(&dest)?;
+    allocate_file(&outfd, len)?;
+
+    for off in 0..blocks {
+        let op = CopyOp {
+            from: source.clone(),
+            to: dest.clone(),
+            start: off * bsize,
+            bytes: cmp::min(len - (off * bsize), bsize)
+        };
+        //debug!("SENDING {:?}", op);
+        work_tx.send(op)?;
+    }
+    drop(work_tx);
+
+    for h in thandles {
+        let _ = h.join();
+    }
+
+    Ok(())
+}
+
 
 pub fn copy_all(sources: Vec<PathBuf>, dest: PathBuf, opts: &Opts) -> Result<()> {
     panic!()
 }
 
-
-pub fn copy_single_file(source: &PathBuf, dest: PathBuf, opts: &Opts) -> Result<()> {
-    panic!()
-}
