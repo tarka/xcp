6661ac46e50afe158fb27cde9bdc5077fa99e47f
Steve Smith <tarkasteve@gmail.com>
2020-08-15 16:56:25 +1000

    Add per-os conditional compilation.

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -33,47 +33,7 @@ pub trait FsOperations {
     fn next_sparse_segments(infd: &File, outfd: &File, pos: u64) -> Result<(u64, u64)>;
 }
 
-struct Linux;
 struct Common;
-
-impl FsOperations for Linux {
-    #[inline(always)]
-    fn allocate_file(fd: &File, len: u64) -> Result<()> {
-        common::allocate_file(fd, len)
-    }
-
-    #[inline(always)]
-    fn copy_permissions(hdl: &CopyHandle, opts: &Opts) -> Result<()> {
-        common::copy_permissions(hdl, opts)
-    }
-
-    #[inline(always)]
-    fn copy_file_bytes(infd: &File, outfd: &File, bytes: u64) -> Result<u64> {
-        linux::copy_file_bytes(infd, outfd, bytes)
-    }
-
-    #[inline(always)]
-    fn copy_file_offset(infd: &File, outfd: &File, bytes: u64, off: i64) -> Result<u64> {
-        linux::copy_file_offset(infd, outfd, bytes, off)
-    }
-
-    #[inline(always)]
-    fn probably_sparse(fd: &File) -> Result<bool> {
-        linux::probably_sparse(fd)
-    }
-
-    #[inline(always)]
-    fn map_extents(fd: &File) -> Result<Vec<Range<u64>>> {
-        linux::map_extents(fd)
-    }
-
-    #[inline(always)]
-    fn next_sparse_segments(infd: &File, outfd: &File, pos: u64) -> Result<(u64, u64)> {
-        linux::next_sparse_segments(infd, outfd, pos)
-    }
-
-}
-
 impl FsOperations for Common {
     #[inline(always)]
     fn allocate_file(fd: &File, len: u64) -> Result<()> {
@@ -112,10 +72,56 @@ impl FsOperations for Common {
 
 }
 
-use cfg_if::cfg_if;
 cfg_if! {
     if #[cfg(any(target_os = "linux", target_os = "android"))] {
         mod linux;
+
+        struct Linux;
+        impl FsOperations for Linux {
+            #[inline(always)]
+            fn allocate_file(fd: &File, len: u64) -> Result<()> {
+                common::allocate_file(fd, len)
+            }
+
+            #[inline(always)]
+            fn copy_permissions(hdl: &CopyHandle, opts: &Opts) -> Result<()> {
+                common::copy_permissions(hdl, opts)
+            }
+
+            #[inline(always)]
+            fn copy_file_bytes(infd: &File, outfd: &File, bytes: u64) -> Result<u64> {
+                linux::copy_file_bytes(infd, outfd, bytes)
+            }
+
+            #[inline(always)]
+            fn copy_file_offset(infd: &File, outfd: &File, bytes: u64, off: i64) -> Result<u64> {
+                linux::copy_file_offset(infd, outfd, bytes, off)
+            }
+
+            #[inline(always)]
+            fn probably_sparse(fd: &File) -> Result<bool> {
+                linux::probably_sparse(fd)
+            }
+
+            #[inline(always)]
+            fn map_extents(fd: &File) -> Result<Vec<Range<u64>>> {
+                linux::map_extents(fd)
+            }
+
+            #[inline(always)]
+            fn next_sparse_segments(infd: &File, outfd: &File, pos: u64) -> Result<(u64, u64)> {
+                linux::next_sparse_segments(infd, outfd, pos)
+            }
+
+        }
+    }
+}
+
+
+use cfg_if::cfg_if;
+cfg_if! {
+    if #[cfg(any(target_os = "linux", target_os = "android"))] {
+//        mod linux;
         pub use common::{
             allocate_file,
             copy_permissions,
