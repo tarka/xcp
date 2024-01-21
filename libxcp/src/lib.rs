53adf1f81265cb3688b09bba1564cb46659c6805
Steve Smith <tarkasteve@gmail.com>
2024-01-21 17:51:21 +1100

    Add simple usage test and corresponding doc example.

--- libxcp/src/lib.rs
+++ libxcp/src/lib.rs
@@ -19,6 +19,47 @@
 //! drivers, and `.gitignore` filters. `libxcp` is the core
 //! functionality of the [xcp] command-line utility.
 //!
+//! # Usage example
+//!
+//! ```
+//! # use std::path::PathBuf;
+//! # use std::sync::Arc;
+//! # use tempfile::TempDir;
+//! #
+//! use libxcp::errors::Result;
+//! use libxcp::config::Config;
+//! use libxcp::operations::StatusUpdate;
+//! use libxcp::{operations::{ChannelUpdater, StatusUpdater}, drivers::{Drivers, load_driver}};
+//!
+//! let sources = vec![PathBuf::from("src")];
+//! let dest = TempDir::new().unwrap();
+//!
+//! let config = Arc::new(Config::default());
+//! let updater = ChannelUpdater::new(&config);
+//! let stat_rx = updater.rx_channel();
+//! let stats: Arc<dyn StatusUpdater> = Arc::new(updater);
+//!
+//! let driver = load_driver(&Drivers::ParFile, &config).unwrap();
+//!
+//! driver.copy_all(sources, dest.path(), stats).unwrap();
+//!
+//! // Gather the results as we go; our end of the channel has been
+//! // moved to the driver call and will end when drained.
+//! for stat in stat_rx {
+//!     match stat {
+//!         StatusUpdate::Copied(v) => {
+//!             println!("Copied {} bytes", v);
+//!         },
+//!         StatusUpdate::Size(v) => {
+//!             println!("Size update: {}", v);
+//!         },
+//!         StatusUpdate::Error(e) => {
+//!             panic!("Error during copy: {}", e);
+//!         }
+//!     }
+//! }
+//! println!("Copy complete");
+//! ```
 //! [xcp]: https://crates.io/crates/xcp/
 
 pub mod config;
@@ -26,3 +67,54 @@ pub mod drivers;
 pub mod errors;
 pub mod operations;
 pub mod paths;
+
+#[cfg(test)]
+#[allow(unused)]
+mod tests {
+    use std::path::PathBuf;
+    use std::sync::Arc;
+
+    use tempfile::TempDir;
+
+    use crate::errors::Result;
+    use crate::config::Config;
+    use crate::operations::StatusUpdate;
+    use crate::{operations::{ChannelUpdater, StatusUpdater}, drivers::{Drivers, load_driver}};
+
+
+    #[test]
+    fn example_usage_test() -> Result<()> {
+        let sources = vec![PathBuf::from("src")];
+        let dest = TempDir::new()?;
+
+        let config = Arc::new(Config::default());
+        let updater = ChannelUpdater::new(&config);
+        let stat_rx = updater.rx_channel();
+        let stats: Arc<dyn StatusUpdater> = Arc::new(updater);
+
+        let driver = load_driver(&Drivers::ParFile, &config)?;
+
+        driver.copy_all(sources, dest.path(), stats)?;
+
+        // Gather the results as we go; our end of the channel has been
+        // moved to the driver call and will end when drained.
+        for stat in stat_rx {
+            match stat {
+                StatusUpdate::Copied(v) => {
+                    println!("Copied {} bytes", v);
+                },
+                StatusUpdate::Size(v) => {
+                    println!("Size update: {}", v);
+                },
+                StatusUpdate::Error(e) => {
+                    println!("Error during copy: {}", e);
+                    return Err(e.into());
+                }
+            }
+        }
+
+        println!("Copy complete");
+
+        Ok(())
+    }
+}
